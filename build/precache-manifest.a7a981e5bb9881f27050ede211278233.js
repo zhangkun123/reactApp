92f71691b12b063e66de0ef39afd2eb772f18d34
zhangkun123 <zhangkun2008204185@gmail.com>
2020-04-07 23:00:27 -0400

    build app

---
+++ build/precache-manifest.a7a981e5bb9881f27050ede211278233.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93a6b1735401bea193db838cb43de39",
+    "url": "/reactApp/index.html"
+  },
+  {
+    "revision": "4f7a41f5c2e6dcc6deec",
+    "url": "/reactApp/static/css/main.b0dd50a7.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/reactApp/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactApp/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f7a41f5c2e6dcc6deec",
+    "url": "/reactApp/static/js/main.1a9799fc.chunk.js"
+  },
+  {
+    "revision": "bcdf5d34fa85d5c25874",
+    "url": "/reactApp/static/js/runtime-main.246891c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
